0fd4181f11dc6dde0dcd7108d51a1abfce252f60
Frankie Yan <frankie.yan@gogotech.hk>
2019-12-11 19:02:16 +0800

    Add populating large array as second example

--- index.mjs
+++ index.mjs
@@ -1,15 +1,24 @@
 import { runAnimation } from './animation.mjs'
 
+const worker = new Worker('worker.js')
+
 const fibMainButton = document.querySelector('#fib-main')
 const fibWorkerButton = document.querySelector('#fib-worker')
 const fibInput = document.querySelector('#fib-input')
 const fibResults = document.querySelector('#fib-results')
-const worker = new Worker('worker.js')
+
+const arrayMainButton = document.querySelector('#array-main')
+const arrayWorkerButton = document.querySelector('#array-worker')
+const arrayInput = document.querySelector('#array-input')
+const arrayResults = document.querySelector('#array-results')
 
 runAnimation()
 
 fibMainButton.addEventListener('click', handleFibMain)
 fibWorkerButton.addEventListener('click', handleFibWorker)
+
+arrayMainButton.addEventListener('click', handleArrayMain)
+arrayWorkerButton.addEventListener('click', handleArrayWorker)
 
 function handleFibMain() {
   fibResults.innerHTML = ''
@@ -29,12 +38,41 @@ function handleFibWorker() {
   const startTime = performance.now()
 
   worker.onmessage = function onReceiveFibResultsFromWorker(event) {
-    const { data: result } = event
+    const { type, result } = event.data
+    if (type !== 'FIBONACCI') return
+
     console.log(`Completed in: ${performance.now() - startTime}ms`)
     fibResults.innerHTML = result
   }
 
-  worker.postMessage({ length: fibInput.value })
+  worker.postMessage({ type: 'FIBONACCI', payload: fibInput.value })
+}
+
+function handleArrayMain() {
+  arrayResults.innerHTML = ''
+
+  setTimeout(() => {
+    const startTime = performance.now()
+    const result = populateArray(arrayInput.value)
+    console.log(`Completed in: ${performance.now() - startTime}ms`)
+
+    arrayResults.innerHTML = result
+  }, 20)
+}
+
+function handleArrayWorker() {
+  arrayResults.innerHTML = ''
+  const startTime = performance.now()
+
+  worker.onmessage = function onReceiveArrayResultsFromWorker(event) {
+    const { type, result } = event.data
+    if (type !== 'POPULATE_ARRAY') return
+
+    console.log(`Completed in: ${performance.now() - startTime}ms`)
+    arrayResults.innerHTML = result
+  }
+
+  worker.postMessage({ type: 'POPULATE_ARRAY', payload: arrayInput.value })
 }
 
 // O(2^n) of death
@@ -44,4 +82,10 @@ function fibonacci(n) {
   }
 
   return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+function populateArray(size) {
+  return Array.from(new Array(parseInt(size, 10)))
+    .map((_, index) => size - index)
+    .join(', ')
 }
--- worker.js
+++ worker.js
@@ -1,7 +1,20 @@
 addEventListener('message', event => {
-  const { length } = event.data
-  const result = fibonacci(length)
-  postMessage(result)
+  const { type, payload } = event.data
+
+  switch (type) {
+    case 'FIBONACCI': {
+      const result = fibonacci(payload)
+      return postMessage({ type: 'FIBONACCI', result })
+    }
+
+    case 'POPULATE_ARRAY': {
+      const result = populateArray(payload)
+      return postMessage({ type: 'POPULATE_ARRAY', result })
+    }
+
+    default:
+      return
+  }
 })
 
 // O(2^n) of death
@@ -11,4 +24,10 @@ function fibonacci(n) {
   }
 
   return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+function populateArray(size) {
+  return Array.from(new Array(parseInt(size, 10)))
+    .map((_, index) => size - index)
+    .join(', ')
 }
